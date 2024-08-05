cc0b0efc3f59fe3660c9972dcbe47113554c2406
Sergey <koblin@ya.ru>
2024-08-05 21:49:46 +0300

    process multiple stations

--- main.py
+++ main.py
@@ -1,25 +1,65 @@
 import json
 import locale
+import sys
 from datetime import datetime, timedelta
-from typing import Optional
+from typing import List, Optional
 
 import pytz
 import requests
 import yaml
 from loguru import logger
 from pod2gen import Category, Episode, Funding, Media, Person, Podcast
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl, ValidationError
+
+locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
+
+logger.remove()
+logger.add(
+    sys.stdout,
+    format="{time:YYYY-MM-DD HH:mm:ss} | [<level>{level}</level>] | {message}",
+    level="DEBUG",
+    colorize=True,
+)
+logger.level("DEBUG", color="<cyan>")
+logger.level("INFO", color="<green>")
+logger.level("WARNING", color="<yellow>")
+logger.level("ERROR", color="<red>")
+logger.level("CRITICAL", color="<magenta>")
 
 
-class PodcastItem(BaseModel):
+class PodcastModel(BaseModel):
+    title: str
+    description: str
+    brand_id: Optional[int] = None
+    rubric_id: Optional[int] = None
+    station: str = Field(default="")
+    category: str
+    sub_category: Optional[str] = None
+    website: HttpUrl
+    feed: str
+    image: HttpUrl
+
+
+class StationModel(BaseModel):
+    name: str
+    id: int
+    website: HttpUrl
+    podcasts: List[PodcastModel]
+
+
+class StationsDataModel(BaseModel):
+    stations: List[StationModel]
+
+
+class EpisodeModel(BaseModel):
     id: int
     brand_id: Optional[int] = None
     rubric_id: Optional[int] = None
     title: str
+    anons: str = Field(default="")
     description: str = Field(default="")
     published: datetime
     duration: int
-    anons: str = Field(default="")
     media_url: str
     media_size: int
     picture_url: str
@@ -43,34 +83,37 @@ def parse_api_date(api_response: str) -> datetime:
 
 
 def get_media_size(url: str) -> int:
-    try:
-        response = requests.head(url, allow_redirects=True)
+    response = requests.head(url, allow_redirects=True)
+    if response.status_code == 200:
         return int(response.headers.get("Content-Length", 0))
-    except requests.exceptions.RequestException as e:
-        logger.error(f"Error getting media size: {e}")
+    else:
+        logger.error(
+            f"Error getting media size: HTTP status code {response.status_code}"
+        )
         return 0
 
 
-def create_podcast(podcast_info):
-    p = Podcast()
-    p.name = podcast_info["title"]
-    p.description = podcast_info["description"]
-    p.category = Category("News", "Politics")
-    p.website = "https://smotrim.ru/radiovesti"
-    p.authors = [Person("Вести ФМ")]
-    p.owner = Person("Sergey", "me@coyotle.ru")
-    p.explicit = False
-    p.language = "ru-RU"
-    p.image = podcast_info.get("image", "")
+def build_p2g_podcast(podcast: PodcastModel):
+    pcast = Podcast()
+    pcast.name = podcast.title
+    pcast.description = podcast.description
+    pcast.category = Category(podcast.category, podcast.sub_category)
+    pcast.website = str(podcast.website)
+    pcast.authors = [Person(podcast.station)]
+    pcast.owner = Person("Sergey", "me@coyotle.ru")
+    pcast.explicit = False
+    pcast.language = "ru-RU"
+    pcast.image = str(podcast.image)
+
     funding = Funding(
         "Поддержите обновление подкаста", "https://pay.cloudtips.ru/p/a368e9f8"
     )
-    p.add_funding(funding)
-    p.generator = "RSS podcast generator v0.1"
-    return p
+    pcast.add_funding(funding)
+    pcast.generator = "Smotrim podcast generator v0.2"
+    return pcast
 
 
-def create_episode(podcast_item):
+def build_p2g_episode(podcast_item):
     ep = Episode()
     ep.title = podcast_item.anons
     ep.long_summary = podcast_item.description
@@ -91,84 +134,110 @@ def create_episode(podcast_item):
     return ep
 
 
-def get_podcast_episodes(podcast_id, is_rubric=False, limit=20):
-    url = "https://smotrim.ru/api/audios"
-    params = {
+def fetch_raw_episodes(podcast: PodcastModel, limit=20) -> List[dict]:
+    episode_url = "https://smotrim.ru/api/audios"
+    episode_params = {
         "page": 1,
         "limit": limit,
-        "rubricId" if is_rubric else "brandId": podcast_id,
     }
+    if podcast.brand_id:
+        episode_params["brandId"] = podcast.brand_id
+    elif podcast.rubric_id:
+        episode_params["rubricId"] = podcast.rubric_id
 
-    r = requests.get(url, params=params)
-    episodes = json.loads(r.text)["contents"][0]["list"]
-    return [item for item in episodes if not item["isActive"]]
+    r = requests.get(episode_url, params=episode_params)
+    return json.loads(r.text)["contents"][0]["list"]
 
 
-def process_podcast(podcast_info):
-    brand_id = podcast_info.get("brand_id")
-    rubric_id = podcast_info.get("rubric_id")
-
-    if not (brand_id or rubric_id):
-        raise ValueError("Neither brand_id nor rubric_id is provided in podcast_info")
-
-    episodes = get_podcast_episodes(brand_id or rubric_id, is_rubric=bool(rubric_id))
-    podcast = create_podcast(podcast_info)
-
-    podcast = create_podcast(podcast_info)
-
-    for item in episodes:
-        media_url = f"https://vgtrk-podcast.cdnvideo.ru/audio/listen?id={item['id']}"
+def process_raw_episodes(
+    raw_episodes: List[dict], podcast: PodcastModel
+) -> List[EpisodeModel]:
+    episodes = []
+    for raw_ep in raw_episodes:
+        media_url = f"https://vgtrk-podcast.cdnvideo.ru/audio/listen?id={raw_ep['id']}"
         media_size = get_media_size(media_url)
 
         if media_size <= 0:
-            logger.warning(f"Skipping episode {item['id']} due to invalid media size")
+            logger.warning(f"Skipping episode {raw_ep['id']} due to invalid media size")
             continue
 
-        description = item["description"] or ""
-        podcast_item = PodcastItem(
-            id=item["id"],
-            brand_id=podcast_info.get("brand_id", None),
-            rubric_id=podcast_info.get("rubric_id", None),
-            title=item["title"],
-            published=parse_api_date(item["published"]),
-            duration=str_to_sec(item["duration"]),
-            anons=item["anons"],
+        resp = requests.get(raw_ep["player"]["preview"]["source"]["main"])
+        picture_url = resp.url
+
+        description = raw_ep["description"] or ""
+        ep = EpisodeModel(
+            id=raw_ep["id"],
+            brand_id=podcast.brand_id,
+            rubric_id=podcast.rubric_id,
+            title=raw_ep["title"],
+            published=parse_api_date(raw_ep["published"]),
+            duration=str_to_sec(raw_ep["duration"]),
+            anons=raw_ep["anons"],
             description=f"{description}\n\nПоддержите обновление подкаста:\nhttps://pay.cloudtips.ru/p/a368e9f8",
             media_url=media_url,
             media_size=media_size,
-            picture_url=item["player"]["preview"]["source"]["main"],
+            picture_url=picture_url,
         )
+        episodes.append(ep)
 
-        episode = create_episode(podcast_item)
-        podcast.episodes.append(episode)
+    return episodes
 
-    return podcast
+
+def generate_podcast_feed(podcast: PodcastModel) -> Podcast:
+    if not (podcast.brand_id or podcast.rubric_id):
+        raise ValueError("Neither brand_id nor rubric_id is provided in podcast_info")
+
+    raw_episodes = fetch_raw_episodes(podcast)
+    episodes = process_raw_episodes(raw_episodes, podcast)
+
+    p2g_podcast = build_p2g_podcast(podcast)
+    for ep in episodes:
+        p2g_episode = build_p2g_episode(ep)
+        p2g_podcast.episodes.append(p2g_episode)
+
+    return p2g_podcast
+
+
+def write_podcast_feed_to_file(podcast: PodcastModel, feed_str: str):
+    filename = podcast.feed
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(feed_str)
+
+        id = podcast.brand_id
+        if podcast.rubric_id:
+            id = podcast.rubric_id
+
+        logger.info(f"-- {id:<6} {podcast.title:<24} \t {filename}")
+    except Exception as e:
+        logger.error(f"Error processing podcast {podcast.title}: {e}")
+
+
+def create_station_feeds(station: StationModel):
+    for podcast in station.podcasts:
+        podcast.station = station.name
+        podcast_feed = generate_podcast_feed(podcast)
+        feed_str = podcast_feed.rss_str()
+        write_podcast_feed_to_file(podcast, feed_str)
 
 
 def main():
-    locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
-
     try:
         with open("podcasts.yaml", "r") as file:
-            podcasts = yaml.safe_load(file)
+            yaml_data = yaml.safe_load(file)
     except Exception as e:
         logger.error(f"Failed to load podcasts.yaml: {e}")
-        return
+        sys.exit(1)
 
-    for podcast_info in podcasts:
-        try:
-            podcast = process_podcast(podcast_info)
-            rss_feed = podcast.rss_str()
+    try:
+        stations_data = StationsDataModel(**yaml_data)
+    except ValidationError as e:
+        logger.error(f"Data validation problem: {e}")
+        sys.exit(1)
 
-            filename = podcast_info["feed"]
-            with open(filename, "w", encoding="utf-8") as file:
-                file.write(rss_feed)
-            logger.info(f"Generated RSS feed for {podcast_info['title']}: {filename}")
-        except Exception as e:
-            logger.error(f"Error processing podcast {podcast_info['title']}: {e}")
-            continue
-
-    logger.info("Finished processing all podcasts")
+    for station in stations_data.stations:
+        logger.info(f"- {station.name}")
+        create_station_feeds(station)
 
 
 if __name__ == "__main__":
