fee29d40cc92105d5f4fa916566434e5f465d756
Sergey <koblin@ya.ru>
2024-08-27 12:48:21 +0300

    fix podcasts id selector

--- get_json.sh
+++ get_json.sh
@@ -2,7 +2,7 @@
 
 LIMIT=100
 
-yq e '.[] | select(.brand_id != null) | .brand_id' podcasts.yaml | while read -r brand_id
+yq e '.stations[].podcasts[] | select(.brand_id != null) | .brand_id' podcasts.yaml | while read -r brand_id
 do
     echo "Get json for brand: $brand_id"
     url="https://smotrim.ru/api/audios?brandId=$brand_id&page=1&limit=$LIMIT"
@@ -10,7 +10,7 @@ do
     curl -s "$url" > ./docs/data/brands/$brand_id.json
 done
 
-yq e '.[] | select(.rubric_id != null) | .rubric_id' podcasts.yaml | while read -r rubric_id
+yq e '.stations[].podcasts[] | select(.rubric_id != null) | .rubric_id' podcasts.yaml | while read -r rubric_id
 do
     echo "Get json for rubric: $rubric_id"
     url="https://smotrim.ru/api/audios?rubricId=$rubric_id&page=1&limit=$LIMIT"
