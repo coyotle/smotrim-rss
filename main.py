c7c3472d964ed7bd5cb20610cc2f90caee5e07eb
Sergey <koblin@ya.ru>
2025-12-15 18:46:18 +0300

    disable compression for http request

--- main.py
+++ main.py
@@ -145,8 +145,12 @@ def fetch_raw_episodes(podcast: PodcastModel, limit=10) -> List[dict]:
     elif podcast.rubric_id:
         episode_params["rubricId"] = podcast.rubric_id
 
+    headers = {"Accept-Encoding": "identity"}
+
     try:
-        r = requests.get(episode_url, params=episode_params, stream=True)
+        r = requests.get(
+            episode_url, params=episode_params, headers=headers, stream=True
+        )
         if r.status_code != 200:
             raise ValueError(f"HTTP status code {r.status_code}")
 
